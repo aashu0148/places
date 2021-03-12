6d15254c5f02b654d28070dcecf6f281cb6e6e58
aashu0148 <buildforfb@gmail.com>
2021-03-11 21:22:46 -0800

    users added

--- frontend/src/components/users/User.js
+++ frontend/src/components/users/User.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
 
-function user() {
-  return <div></div>;
+import "./User.css";
+
+function user(props) {
+  return (
+    <div className="user">
+      <div className="user_head">
+        <Avatar src={props.userPhoto} />
+        <h3 className="user_title">{props.name}</h3>
+      </div>
+      <div className="user_places">{props.places} Places</div>
+    </div>
+  );
 }
 
 export default user;
--- frontend/src/components/users/Users.js
+++ frontend/src/components/users/Users.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+
+import User from "./User";
+import "./Users.css";
 
 function Users() {
-  return <div className="users"></div>;
+  const [name, setName] = useState("");
+  return (
+    <div className="users">
+      <div className="users_search">
+        <input
+          type="text"
+          placeholder="search user"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <div className="users_search-btn">
+          <SearchIcon />
+        </div>
+      </div>
+      <User
+        name="John"
+        places="12"
+        userPhoto="https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427__340.jpg"
+      />
+      <User
+        name="Suzen"
+        places="7"
+        userPhoto="https://cdn.pixabay.com/photo/2020/10/04/10/43/horse-5625922__340.jpg"
+      />
+      <User
+        name="jimmy"
+        places="4"
+        userPhoto="https://cdn.pixabay.com/photo/2015/09/18/00/24/robin-944887__340.jpg"
+      />
+    </div>
+  );
 }
 
 export default Users;
